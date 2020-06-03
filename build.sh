226d9ead136d79d4401dae50a6912e3e99dbd9b6
pokachopotun <mr.salixnew@gmail.com>
2020-06-03 09:14:42 +0300

    fix compiler

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ TARGET_FOLDER=$(cat target_folder.txt)
 rm -rf build
 mkdir build
 cd build
-cmake .. -DTARGET_FOLDER=${TARGET_FOLDER} -DCMAKE_CXX_COMPILER=g++-9
+cmake .. -DTARGET_FOLDER=${TARGET_FOLDER}
 cmake --build .
 cmake --build . --target test
 cmake --build . --target package
